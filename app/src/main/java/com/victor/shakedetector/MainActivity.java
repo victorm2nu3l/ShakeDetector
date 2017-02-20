341cd9efd9eb64b888d9dbc0cb7fb1309e9e8cb8
Victor Espinoza <victormeg16@gmail.com>
2017-02-20 10:49:01 -0500

    Initital commit

---
+++ app/src/main/java/com/victor/shakedetector/MainActivity.java
@@ -0,0 +1,126 @@
+package com.victor.shakedetector;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getCanonicalName();
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (mAccelerometer != null) {
+
+            mShakeDetector = new ShakeDetector();
+            mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+                @Override
+                public void onShake(int count) {
+
+                    try {
+                        Log.d(TAG, String.format("Shake it:%s", count));
+                        Toast.makeText(MainActivity.this, "Shake it!", Toast.LENGTH_LONG).show();
+                    } catch (Exception ex) {
+                        Log.e(TAG, "", ex);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
+
+    public static class ShakeDetector implements SensorEventListener {
+
+        /*
+     * The gForce that is necessary to register as shake.
+     * Must be greater than 1G (one earth gravity unit).
+     * You can install "G-Force", by Blake La Pierre
+     * from the Google Play Store and run it to see how
+     *  many G's it takes to register a shake
+     */
+        private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+        private static final int SHAKE_SLOP_TIME_MS = 500;
+        private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+
+        private OnShakeListener mListener;
+        private long mShakeTimestamp;
+        private int mShakeCount;
+
+        public void setOnShakeListener(OnShakeListener listener) {
+            this.mListener = listener;
+        }
+
+        public interface OnShakeListener {
+            public void onShake(int count);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            if (mListener != null) {
+                float x = sensorEvent.values[0];
+                float y = sensorEvent.values[1];
+                float z = sensorEvent.values[2];
+
+                float gX = x / SensorManager.GRAVITY_EARTH;
+                float gY = y / SensorManager.GRAVITY_EARTH;
+                float gZ = z / SensorManager.GRAVITY_EARTH;
+
+                // gForce will be close to 1 when there is no movement.
+
+
+                float gForce = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+                if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                    final long now = System.currentTimeMillis();
+                    // ignore shake events too close to each other (500ms)
+                    if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                        return;
+                    }
+
+                    // reset the shake count after 3 seconds of no shakes
+                    if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                        mShakeCount = 0;
+                    }
+
+                    mShakeTimestamp = now;
+                    mShakeCount++;
+
+                    mListener.onShake(mShakeCount);
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    }
+}
